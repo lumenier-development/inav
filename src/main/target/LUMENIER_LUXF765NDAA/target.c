9edcf5a261dfa44e2b989955739adac2aacedd64
D.J. Morvay <djmorvay@me.com>
2025-03-03 13:01:12 -0500

    Pushing initial LUX F765 - NDAA config.

---
+++ src/main/target/LUMENIER_LUXF765NDAA/target.c
@@ -0,0 +1,46 @@
+/*
+ * This file is part of Cleanflight.
+ *
+ * Cleanflight is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Cleanflight is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Cleanflight.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <stdint.h>
+
+#include <platform.h>
+#include "drivers/io.h"
+#include "drivers/pwm_mapping.h"
+#include "drivers/timer.h"
+
+timerHardware_t timerHardware[] = {
+    DEF_TIM(TIM2,  CH1, PA0,  TIM_USE_OUTPUT_AUTO,                  0, 0 ), // M1
+    DEF_TIM(TIM2,  CH2, PA1,  TIM_USE_OUTPUT_AUTO,                  0, 0 ), // M2
+    DEF_TIM(TIM2,  CH3, PA2,  TIM_USE_OUTPUT_AUTO,                  0, 0 ), // M3
+    DEF_TIM(TIM2,  CH4, PA3,  TIM_USE_OUTPUT_AUTO,                  0, 0 ), // M4
+
+    DEF_TIM(TIM4,  CH1, PD12,  TIM_USE_OUTPUT_AUTO,                  0, 0 ), // M5
+    DEF_TIM(TIM4,  CH2, PD13,  TIM_USE_OUTPUT_AUTO,                  0, 0 ), // M6
+    DEF_TIM(TIM4,  CH3, PD14,  TIM_USE_OUTPUT_AUTO,                  0, 0 ), // M7
+    DEF_TIM(TIM4,  CH4, PD15,  TIM_USE_OUTPUT_AUTO,                  0, 0 ), // M8
+   
+    DEF_TIM(TIM3,  CH3, PB0,  TIM_USE_OUTPUT_AUTO,                  0, 0 ), // S1
+    DEF_TIM(TIM3,  CH4, PB1,  TIM_USE_OUTPUT_AUTO,                  0, 0 ), // S2
+    DEF_TIM(TIM1,  CH1, PE9,  TIM_USE_OUTPUT_AUTO,                  0, 0 ), // S3
+    DEF_TIM(TIM1,  CH2, PE11,  TIM_USE_OUTPUT_AUTO,                 0, 0 ), // S4
+
+    DEF_TIM(TIM3,  CH2, PB5,  TIM_USE_OUTPUT_AUTO | TIM_USE_LED,    0, 0 ), // LED
+    DEF_TIM(TIM8,  CH3, PC8,  TIM_USE_BEEPER,                       0, 0 ), // BEEPER
+    // DEF_TIM(TIM1,  CH3, PE13, TIM_USE_PPM,                          0, 0 ), // PPM - NOT INCLUDED!
+};
+
+const int timerHardwareCount = sizeof(timerHardware) / sizeof(timerHardware[0]);
